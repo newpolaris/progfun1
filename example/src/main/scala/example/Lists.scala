bcd4594b619c4b74dc3fb759a57b52fe20b1c6d4
newpolaris <newpolaris@gmail.com>
2017-01-09 14:36:48 +0900

    accept

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -23,7 +23,7 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
+    def sum(xs: List[Int]): Int = if (xs.isEmpty) 0 else xs.head + sum(xs.tail)
   
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +38,9 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = { 
+      if (xs.isEmpty) throw new java.util.NoSuchElementException
+      else if (xs.length == 1) xs(0)
+      else math.max(xs.head, max(xs.tail))
+    }
   }
--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,38 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if (c == 0 || c == r) 1
+      else pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def stack(frame: Int, chars: List[Char]): Boolean = {
+        if (chars.isEmpty) frame == 0
+        else if (chars.head != '(' && chars.head != ')') stack(frame, chars.tail)
+        else if (chars.head == '(') stack(frame+1, chars.tail)
+        else {
+          if (frame <= 0) false
+          else stack(frame-1, chars.tail)
+        }
+      }
+      stack(0, chars)
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      def change(money: Int, coins: List[Int]): Int = {
+        if (money < 0 || coins.isEmpty) 0
+        else if (money == 0) 1
+        else {
+          change(money - coins.head, coins) + change(money, coins.tail)
+        }
+      }
+      change(money, coins.sorted.reverse)
+    }
   }
